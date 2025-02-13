d0ad20d65caf35dacaf3bc269d7f83650481b897
InnovateWithRK <rk.go@icloud.com>
2025-02-13 09:52:50 -0800

    Remove RabbitMQ.Client package reference from Gateway, Notifications, and Payments projects; add it to Shared project and implement RabbitMQ configuration in new StartupExtensions class.

---
+++ CookieStore.Shared/StartupExtensions.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace CookieStore.Shared;
+
+public class RabbitMqOptions
+{
+    public string Scheme { get; set; } = "amqps";
+    public string Host { get; set; } = "";
+    public int Port { get; set; } = 5671;
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string VirtualHost { get; set; } = "/";
+}
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddCookieStoreInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<RabbitMqOptions>(configuration.GetSection("RabbitMq"));
+
+        services.AddSingleton(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+            var uri = new UriBuilder
+            {
+                Scheme = options.Scheme,
+                Host = options.Host,
+                Port = options.Port,
+                UserName = options.Username,
+                Password = options.Password,
+                Path = options.VirtualHost
+            }.Uri;
+            return new ConnectionFactory { Uri = uri };
+        });
+
+        return services;
+    }
+}
